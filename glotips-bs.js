57a478c0983e68ac35a8ca154583ea6842eeabbb
Paul Dominik <iam@pauldominik.com>
2016-10-25 00:01:45 +0800

    added bootstrap support

--- glotips-bs.js
+++ glotips-bs.js
@@ -3,6 +3,7 @@
 
 	$.fn.glotify = function(dictionary, options) {
 		var self = this;
+
 		//dictionary || dictionary = {};
 		if (typeof dictionary == 'undefined' || $.isEmptyObject(dictionary)) {
 			throw new Error('Glotip:dictionary is either empty or undefined');
@@ -11,13 +12,25 @@
 
 		// options
 		var settings = $.extend({
-			tipsCssNamespace: 'glotip',
-			tags:'p, li', // tags to scrape, inner tags will automatically be affected
-			tagsUntouched: '', // todo: specify tags where inner text will not be handled, use only if these tags expected to be childrend of tags listed in tags property in this config/settings
+			tipsClass: 'glotip', // class added to texts that launch a popover, note that they are dyn created <a> tags
+			tags:'p, li', // these tags' innerText will be scanned, inner tags, like <span> or <a> are ignored
+			oncePerTag: true,
+
+			// these are just boostrap popover options, http://getbootstrap.com/javascript/#popovers
+			placement: 'auto top',
+			trigger: 'focus',
+			viewport: '.article-body',
+			animation: true,
+			template: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title">hey</h3><div class="popover-content"></div></div>',
 		}, options );
 
 		// selector
 		var $contentToGlotify = this;
+
+		// we have to make sure that the container has position:relative as popover will use percentage width
+		$contentToGlotify.css('position', 'relative');
+
+		this.settings = settings;
 
 		// methods
 		this.getDictionary = function(){
@@ -37,33 +50,25 @@
 				// use jquery convenience
 				var $em = $(htmlEm);
 				var text = $em.html();
-
+				
 				// iterate on each dictionary term
 				$.each(dictionary, function(dTerm, dDefinition){
-					var matchHandler = function(p1, p2) {
-						//console.log('p1', p1, 'p2', p2);
-						return ((p2==undefined)||p2=='')?p1:'<span data-toggle="popover" title="'+dTerm+'" data-content="'+dDefinition+'" class="'+settings.tipsCssNamespace+'" title="'+dDefinition+'">'+p1+'</span>';
-					}
-					//text = text.replace(new RegExp('(?!<span data-tooltip[^>]*?>)('+dTerm+')(?![^<]*?</span>)', 'gi'), '<span data-tooltip class="has-tip '+settings.tipsCssNamespace+'" title="'+dDefinition+'">$1</span>');
 
-					// don't recurse if phrase/key is used in the glossary definition, i.e. within the data-tooltip tag: (?!<span data-tooltip[^>]*?>)
-					// don't touch matching terms within html tags
-					//text = text.replace(new RegExp('<[^>]+>|(?!<span data-tooltip[^>]*?>)('+dTerm+')(?![^<]*?</span>)', 'gi'), matchHandler);
-					//text = text.replace(new RegExp('<[^>]+>|('+dTerm+')', 'gi'), matchHandler);
-					//text = text.replace(new RegExp('<[^>]+>|(?!<em[^>]*?>)('+dTerm+')(?![^<]*?<\/em>)', 'gi'), matchHandler);
-					text = text.replace(new RegExp('<[^>]+>|('+dTerm+')', 'gi'), matchHandler);
+					var matchHandler = function(p1, p2) {
+						tabindex = 0;
+						return ((p2==undefined)||p2=='')?p1:'<a data-toggle="popover" tabindex="'+(++tabindex)+'" role="button" data-trigger="focus" title="'+dTerm+'" data-content="'+dDefinition+'" class="'+settings.tipsClass+'" title="'+dDefinition+'">'+p1+'</a>';
+					}
+					var regexFlag = settings.oncePerTag?'i':'gi';
+					text = text.replace(new RegExp('('+dTerm+')(?![^<]*>|[^<>]*<\/)', regexFlag), matchHandler);
 				})
 				$em.html(text);
-
 			}
 			else
 			{
 				throw new Error('Please pass in an instanceof HTMLElement only')
 			}
-
 			return $em.html();
 		};
-
 
 		// select all elements we are interested in, limit our context within .content
 		$selectedEms = $contentToGlotify.find(settings.tags);
@@ -73,22 +78,8 @@
 			self.glotifyElement(this);
 		})
 
-		// after all is done, make sure tooltips are reinitialized
-		//$(document).foundation()
-		$('[data-toggle="popover"]')
-		.popover({
-			'placement': 'auto bottom',
-			'trigger': 'click',
-			'animation':true,
-			'container': '#article-body',
-			'viewport': '#article-body',
-			'template': '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title">hey</h3><div class="popover-content"></div></div>'})
-		.on('click', function (e) {
-			// just one visible popover each time
-			$('[data-toggle="popover"]').not(this).popover('hide');
-		});
-		
-
+		// initialize them all
+		$('[data-toggle="popover"]').popover(settings);
 
 		// leak global
 		return window.glotips = this;
@@ -100,15 +91,14 @@
 //load it here instead
 jQuery(document).ready(function($){
 
-	$.getJSON( "dictionary.json", function( data ) {
+	$.getJSON( "/hc/theme_assets/135579/200062933/dictionary.json", function( data ) {
 		var dictionary = {};
 		$.each( data.dictionary, function( i, obj ) {
 			dictionary[obj.term] = obj.def;
 		});
 
-		$('.article-body').glotify(dictionary);
-		console.log(dictionary);
-	});
+		$('.article-body').glotify(dictionary, {viewport: '.article-body', tags: 'p,li'});
 
+	});
 
 });
---
+++ hc/theme_assets/135579/200062933/glotips-bs.js
@@ -0,0 +1,113 @@
+//jQuery.noConflict();
+(function initGlotips($, glotips) {
+
+	$.fn.glotify = function(dictionary, options) {
+		var self = this;
+		//dictionary || dictionary = {};
+		if (typeof dictionary == 'undefined' || $.isEmptyObject(dictionary)) {
+			throw new Error('Glotip:dictionary is either empty or undefined');
+			exit;
+		}
+
+		// options
+		var settings = $.extend({
+			tipsCssNamespace: 'glotip',
+			tags:'p, li', // tags to scrape, inner tags will automatically be affected
+			tagsUntouched: '', // todo: specify tags where inner text will not be handled, use only if these tags expected to be childrend of tags listed in tags property in this config/settings
+		}, options );
+
+		// selector
+		var $contentToGlotify = this;
+
+		// methods
+		this.getDictionary = function(){
+			return dictionary;
+		}
+		/**
+		 * Glotify single node
+		 *
+		 * @param htmlEm, HTMLElement
+		 * @var jQuery
+		 */
+		this.glotifyElement = function glotifyElement(htmlEm){
+
+			// only do it if passed an HTMLElement
+			if (htmlEm instanceof HTMLElement) 
+			{
+				// use jquery convenience
+				var $em = $(htmlEm);
+				var text = $em.html();
+
+				// iterate on each dictionary term
+				$.each(dictionary, function(dTerm, dDefinition){
+					var matchHandler = function(p1, p2) {
+						//console.log('p1', p1, 'p2', p2);
+						return ((p2==undefined)||p2=='')?p1:'<span data-toggle="popover" title="'+dTerm+'" data-content="'+dDefinition+'" class="'+settings.tipsCssNamespace+'" title="'+dDefinition+'">'+p1+'</span>';
+					}
+					//text = text.replace(new RegExp('(?!<span data-tooltip[^>]*?>)('+dTerm+')(?![^<]*?</span>)', 'gi'), '<span data-tooltip class="has-tip '+settings.tipsCssNamespace+'" title="'+dDefinition+'">$1</span>');
+
+					// don't recurse if phrase/key is used in the glossary definition, i.e. within the data-tooltip tag: (?!<span data-tooltip[^>]*?>)
+					// don't touch matching terms within html tags
+					//text = text.replace(new RegExp('<[^>]+>|(?!<span data-tooltip[^>]*?>)('+dTerm+')(?![^<]*?</span>)', 'gi'), matchHandler);
+					//text = text.replace(new RegExp('<[^>]+>|('+dTerm+')', 'gi'), matchHandler);
+					//text = text.replace(new RegExp('<[^>]+>|(?!<em[^>]*?>)('+dTerm+')(?![^<]*?<\/em>)', 'gi'), matchHandler);
+					text = text.replace(new RegExp('<[^>]+>|('+dTerm+')', 'gi'), matchHandler);
+				})
+				$em.html(text);
+
+			}
+			else
+			{
+				throw new Error('Please pass in an instanceof HTMLElement only')
+			}
+
+			return $em.html();
+		};
+
+
+		// select all elements we are interested in, limit our context within .content
+		$selectedEms = $contentToGlotify.find(settings.tags);
+
+		// iterate over each element, note the context for 'this' changes within .each
+		$selectedEms.each(function(index){
+			self.glotifyElement(this);
+		})
+
+		// after all is done, make sure tooltips are reinitialized
+		//$(document).foundation()
+		$('[data-toggle="popover"]')
+		.popover({
+			'placement': 'auto bottom',
+			'trigger': 'click',
+			'animation':true,
+			'viewport': '.article-body',
+			'template': '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title">hey</h3><div class="popover-content"></div></div>'})
+		.on('click', function (e) {
+			// just one visible popover each time
+			$('[data-toggle="popover"]').not(this).popover('hide');
+		});
+		
+
+
+		// leak global
+		return window.glotips = this;
+	};
+
+
+})(jQuery, window.glotips || (window.glotips = {}));
+
+//load it here instead
+jQuery(document).ready(function($){
+
+	$.getJSON( "/hc/theme_assets/135579/200062933/dictionary.json", function( data ) {
+		var dictionary = {};
+		$.each( data.dictionary, function( i, obj ) {
+			dictionary[obj.term] = obj.def;
+		});
+
+		$('.article-body').glotify(dictionary);
+		//console.log(dictionary);
+	});
+
+
+});
